3ad30fdf44d558b64bfbd14ad6c4c038c24f3472
Daniel Ellis <up940148@myport.ac.uk>
2022-03-09 12:25:41 +0000

    Reset tree code

--- modeling/trees/generate-tree.js
+++ modeling/trees/generate-tree.js
@@ -15,24 +15,24 @@ import * as shapes from './shapes.js';
 import { P } from './point.js';
 
 function main() {
-  const r = 30;
-  const trunk = 20;
+  const r = 60;
+  const trunk = 30;
 
   const N = 10000;
   const hollowing = shapes.randomHollowing; // makes the generated sphere or half-sphere less dense inside
 
-  // const points = shapes.hollowSphereAttractionPoints(N, r, P(0, 0, trunk + r), hollowing);
-  const points = shapes.hollowHalfSphereAttractionPoints(N, r, P(0, 0, trunk), hollowing);
+  const points = shapes.hollowSphereAttractionPoints(N, r, P(0, 0, trunk + r), hollowing);
+  // const points = shapes.hollowHalfSphereAttractionPoints(N, r, P(0, 0, trunk), hollowing);
   // const points = shapes.nRandomSpheres(5, 1000, r);
 
   const tree = trees.makeTree(points, {
     D: 1,
     di: (r + trunk) * 0.8, // radius of influence
-    dk: 5, // kill distance
+    dk: 6, // kill distance
     relocateFraction: 0.2, // how much down the branch towards the root should every point move
     minThickness: 0.2, // how thick should the smallest branches be
     thicknessPower: 2, // bigger power means smaller branches are closer in thickness to parent branches
-    maxStep: 50,
+    maxStep: 1000,
   });
 
   const leaves = trees.selectLeaves(tree);
